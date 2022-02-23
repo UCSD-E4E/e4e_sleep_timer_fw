afaf5d1c4525f3f9a263d49aa9cbc92793e3d2eb
Frank Chaqueco <fchaqueco01@gmail.com>
2022-02-23 03:26:29 -0800

    Created setState

--- e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_PowerControl.c
+++ e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_PowerControl.c
@@ -9,17 +9,47 @@
 #include <E4E_HAL_PowerControl.h>
 #include <e4e_common.h>
 
+
 int E4E_HAL_PwrCtrl_init(E4E_HAL_PWRCTRLDesc_t *pDesc,
 		E4E_HAL_PWRCTRLDevice_e device, E4E_HAL_PWRCTRLConfig_t *pConfig)
 {
-	return E4E_ERROR;
+	 if (pDesc == pHalDesc)
+	     {
+	         printf("E4E_OK");
+	     }
+	 if (device == E4E_HAL_PWRCTRL_Device_OBC)
+	 {
+		 printf("Powering OBC");
+	 }
 }
+
 int E4E_HAL_PwrCtrl_deinit(E4E_HAL_PWRCTRLDesc_t *pDesc)
 {
-	return E4E_ERROR;
+	if (pDesc == 0) return E4E_ERROR;
+
+	else return E4E_OK;
 }
+
 int E4E_HAL_PwrCtrl_setState(E4E_HAL_PWRCTRLDesc_t *pDesc,
 		E4E_HAL_PWRCTRL_State_e state)
 {
-	return E4E_ERROR;
+
+	if(state == E4E_HAL_PWRCTRL_State_ON)
+		{
+			HAL_GPIO_WritePin(SYS_PWR_GPIO_Port, SYS_PWR_Pin, SET);
+			printf("PWR ON");
+			return E4E_OK;
+		}
+	else if(state == E4E_HAL_PWRCTRL_State_OFF)
+		{
+			HAL_GPIO_WritePin(SYS_PWR_GPIO_Port, SYS_PWR_Pin, RESET);
+			printf("PWR OFF");
+			return E4E_OK;
+		}
+	else
+		{
+			return E4E_ERROR;
+		}
+
+	if (pDesc == 0) return E4E_ERROR;
 }
