80ff7954bd7798729cddaac655b5382cebb598b4
Nathan Hui <ntlhui@gmail.com>
2022-06-01 11:00:57 -0700

    Reenabling other system initialization

--- e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_System.c
+++ e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_System.c
@@ -17,18 +17,18 @@ int E4E_HAL_System_init(void)
 	E4E_HAL_PWRCTRLConfig_t pwrConfig;
 
 
-//	if (E4E_ERROR
-//			== E4E_HAL_Serial_init(&pHalSystem->commandSerialDesc,
-//					E4E_HAL_SerialDevice_Command, &serialConfig))
-//	{
-//		return E4E_ERROR;
-//	}
-//	if (E4E_ERROR
-//			== E4E_HAL_Serial_init(&pHalSystem->debugSerialDesc,
-//					E4E_HAL_SerialDevice_Debug, &serialConfig))
-//	{
-//		return E4E_ERROR;
-//	}
+	if (E4E_ERROR
+			== E4E_HAL_Serial_init(&pHalSystem->commandSerialDesc,
+					E4E_HAL_SerialDevice_Command, &serialConfig))
+	{
+		return E4E_ERROR;
+	}
+	if (E4E_ERROR
+			== E4E_HAL_Serial_init(&pHalSystem->debugSerialDesc,
+					E4E_HAL_SerialDevice_Debug, &serialConfig))
+	{
+		return E4E_ERROR;
+	}
 	if (E4E_ERROR == E4E_HAL_RTC_init(&pHalSystem->rtcDesc, NULL))
 	{
 		return E4E_ERROR;
