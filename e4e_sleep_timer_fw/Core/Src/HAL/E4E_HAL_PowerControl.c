74f414516de558e42f0e70bcb43221bffe7c2d7b
Frank Chaqueco <fchaqueco01@gmail.com>
2022-06-28 21:51:49 -0700

    Fixed stylistic issues

--- e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_PowerControl.c
+++ e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_PowerControl.c
@@ -2,32 +2,39 @@
  * power_control.c
  *
  *  Created on: Feb 9, 2022
- *      Author: ntlhui
+ *      Author: frankc
  */
 
 
 #include <E4E_HAL_PowerControl.h>
+#include <Debug/conio.h>
 #include <e4e_common.h>
 #include <stdio.h>
 #include <main.h>
 
 
-int E4E_HAL_PwrCtrl_init(E4E_HAL_PWRCTRLDesc_t *pDesc, E4E_HAL_PWRCTRLDevice_e device,E4E_HAL_PWRCTRLConfig_t *pConfig)
-{//this function sets the OBC to default status ON unless user wants to initialize as OFF
+/**
+ * @brief Initializes the Power Control Driver (sets to ON)
+ * @param pDesc	Power Control Driver to initialize
+ * @param device	Power Control Device to initialize
+ * @return E4E_OK if successful, otherwise E4E_ERROR
+ */
+int E4E_HAL_PwrCtrl_init(E4E_HAL_PWRCTRLDesc_t *pDesc, E4E_HAL_PWRCTRLDevice_e device, E4E_HAL_PWRCTRLConfig_t *pConfig)
+{
 	if (device == E4E_HAL_PWRCTRL_Device_OBC)
 		{
-			printf("Device: OBC"); //confirmation a valid device was selected
+		E4E_Printf("Device: OBC"); //confirmation a valid device was selected
 			if (pDesc == E4E_HAL_PWRCTRL_State_ON) //power descriptor is selected to be ON
 			{
 				E4E_HAL_PwrCtrl_setState(pDesc, E4E_HAL_PWRCTRL_State_ON); //set state to ON
-				printf("Device: OBC ON"); //confirmation
+				E4E_Printf("Device: OBC ON"); //confirmation
 				return E4E_OK;
 			}
 
 			else //if for some reason pDesc is set as OFF then proceed to set state as OFF
 			{
 				E4E_HAL_PwrCtrl_setState(pDesc, E4E_HAL_PWRCTRL_State_OFF); //set to OFF
-				printf("Device: OBC OFF"); //confirmation
+				E4E_Printf("Device: OBC OFF"); //confirmation
 				return E4E_OK;
 			}
 		}
@@ -39,16 +46,25 @@ int E4E_HAL_PwrCtrl_init(E4E_HAL_PWRCTRLDesc_t *pDesc, E4E_HAL_PWRCTRLDevice_e d
 
 }
 
-int E4E_HAL_PwrCtrl_setState(E4E_HAL_PWRCTRLDesc_t *pDesc, E4E_HAL_PWRCTRL_State_e state)
-{//sets state of output pin to MOSFET
+/**
+ * @brief Sets the Power Control device state (ON or OFF)
+ * @param pDesc	Power Control Driver
+ * @param state	Power Control Device state to set
+ * @return	E4E_OK if successful, otherwise E4E_ERROR
+ */
+int E4E_HAL_PwrCtrl_setState(E4E_HAL_PWRCTRLDesc_t *pDesc,E4E_HAL_PWRCTRL_State_e state)
 
-	if(state == E4E_HAL_PWRCTRL_State_ON && pDesc == E4E_HAL_PWRCTRL_State_ON)
+{
+
+
+
+	if(E4E_HAL_PWRCTRL_State_ON == state)
 		{
 			HAL_GPIO_WritePin(SYS_PWR_GPIO_Port, SYS_PWR_Pin, GPIO_PIN_SET);//sets pin high
 			return E4E_OK;
 
 		}
-	else if(state == E4E_HAL_PWRCTRL_State_OFF && pDesc == E4E_HAL_PWRCTRL_State_OFF)
+	else if(E4E_HAL_PWRCTRL_State_OFF == state)
 		{
 			HAL_GPIO_WritePin(SYS_PWR_GPIO_Port, SYS_PWR_Pin, GPIO_PIN_RESET);//sets pin low
 			return E4E_OK;
@@ -61,18 +77,23 @@ int E4E_HAL_PwrCtrl_setState(E4E_HAL_PWRCTRLDesc_t *pDesc, E4E_HAL_PWRCTRL_State
 }
 
 
-
-int E4E_HAL_PwrCtrl_deinit(E4E_HAL_PWRCTRLDesc_t *pDesc) //sets OBC to OFF
+/**
+ * @brief Deinitializes the Power Control Driver (sets to OFF)
+ * @param pDesc	Power Control Driver to deinitialize
+ * @return	E4E_OK if successful, otherwise E4E_ERROR
+ */
+int E4E_HAL_PwrCtrl_deinit(E4E_HAL_PWRCTRLDesc_t *pDesc)
 {
+
 
 	if (pDesc == E4E_HAL_PWRCTRL_State_OFF) //defaults OBC to OFF
 		{
 			E4E_HAL_PwrCtrl_setState(pDesc, E4E_HAL_PWRCTRL_State_OFF);
-			printf("Device: OBC OFF");
+			E4E_Printf("Device: OBC OFF");
 			return E4E_OK;
 		}
 
-	else
+	else //in the case the OBC fails to de-initialize
 		{
 			return E4E_ERROR;
 		}
