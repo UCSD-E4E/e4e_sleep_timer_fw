e3314e3f67f82bfb603622c0226be47e9fa03a16
Frank Chaqueco <fchaqueco@users.noreply.github.com>
2022-07-19 18:54:53 -0700

    reverted changes

--- e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_System.c
+++ e4e_sleep_timer_fw/Core/Src/HAL/E4E_HAL_System.c
@@ -4,12 +4,9 @@
  *  Created on: Feb 10, 2022
  *      Author: ntlhui
  */
-
 #include <E4E_HAL_System.h>
 #include <e4e_common.h>
-
 E4E_HAL_System_t halSystem, *pHalSystem = &halSystem;
-
 int E4E_HAL_System_init(void)
 {
 	E4E_HAL_SerialConfig_t serialConfig;
@@ -17,27 +14,22 @@ int E4E_HAL_System_init(void)
 	E4E_HAL_PWRCTRLConfig_t pwrConfig;
 
 
-	if (E4E_ERROR
-			== E4E_HAL_Serial_init(&pHalSystem->commandSerialDesc,
-					E4E_HAL_SerialDevice_Command, &serialConfig))
-	{
-		return E4E_ERROR;
-	}
-	if (E4E_ERROR
-			== E4E_HAL_Serial_init(&pHalSystem->debugSerialDesc,
-					E4E_HAL_SerialDevice_Debug, &serialConfig))
-	{
-		return E4E_ERROR;
-	}
-	if (E4E_ERROR == E4E_HAL_RTC_init(&pHalSystem->rtcDesc, &rtcConfig))
-	{
-		return E4E_ERROR;
-	}
+//	if (E4E_ERROR
+//			== E4E_HAL_Serial_init(&pHalSystem->commandSerialDesc,
+//					E4E_HAL_SerialDevice_Command, &serialConfig))
+//	{
+//		return E4E_ERROR;
+//	}
+//	if (E4E_ERROR
+//			== E4E_HAL_Serial_init(&pHalSystem->debugSerialDesc,
+//					E4E_HAL_SerialDevice_Debug, &serialConfig))
+//	{
+//		return E4E_ERROR;
+//	}
+//	if (E4E_ERROR == E4E_HAL_RTC_init(&pHalSystem->rtcDesc, &rtcConfig))
+//	{
+//		return E4E_ERROR;
+//	}
 	if (E4E_ERROR
 			== E4E_HAL_PwrCtrl_init(&pHalSystem->onboardComputerDesc,
 					E4E_HAL_PWRCTRL_Device_OBC, &pwrConfig))
-	{
-		return E4E_ERROR;
-	}
-	return E4E_OK;
-}
